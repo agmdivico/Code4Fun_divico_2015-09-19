861813338728ac6a92acb4c7f9be4d135cc8868a
agmdivico <a.divico@ymail.com>
2015-09-21 00:42:24 +0200

    Begin Test

---
+++ workspace/new_project_ADV/src/new_project_ADV/ListNodeNTest.java
@@ -0,0 +1,19 @@
+package new_project_ADV;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListNodeNTest {
+
+	@Test
+	public void testListNodeN() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNode() {
+		fail("Not yet implemented");
+	}
+
+}
